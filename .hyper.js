d40b3499348f4fc4652e3a4205c4e358cefe2ccd
yochem <yochem@users.noreply.github.com>
2018-03-19 18:08:38 +0100

    Added dracula package to Hyper

--- .hyper.js
+++ .hyper.js
@@ -105,7 +105,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-alt-click'],
+  plugins: [
+    'hyper-alt-click',
+    'hyper-dracula'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
