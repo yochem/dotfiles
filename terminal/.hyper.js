e4e9ab1a494c22530b75dea95a12595cbd2ccda5
Yochem van Rosmalen <yochem+git@icloud.com>
2018-05-11 20:40:29 +0200

    broadcast pluging

--- terminal/.hyper.js
+++ terminal/.hyper.js
@@ -105,10 +105,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    'hyper-alt-click',
-    'hyper-dracula'
-  ],
+  plugins: ['hyper-alt-click', 'hyper-dracula', "hyper-broadcast"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
