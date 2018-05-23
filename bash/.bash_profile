a77e69591b0cdc2bc84657dbeb8164cd4d3b2258
yochem <yochem@users.noreply.github.com>
2018-05-23 21:23:25 +0200

    TABS

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -4,7 +4,7 @@
 
 # Load the shell dotfiles, and then some:
 for file in ~/.{aliases,bash_prompt,exports,functions}; do
-  [ -r "$file" ] && [ -f "$file" ] && source "$file";
+	[ -r "$file" ] && [ -f "$file" ] && source "$file";
 done;
 unset file;
 
@@ -19,9 +19,9 @@ shopt -s cdspell;
 
 # Add tab completion for many Bash commands
 if which brew &> /dev/null && [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ]; then
-  source "$(brew --prefix)/share/bash-completion/bash_completion";
+	source "$(brew --prefix)/share/bash-completion/bash_completion";
 elif [ -f /etc/bash_completion ]; then
-  source /etc/bash_completion;
+	source /etc/bash_completion;
 fi;
 
 # sourcing z
--- terminals/.hyper.js
+++ terminals/.hyper.js
@@ -3,112 +3,112 @@
 // See https://hyper.is#cfg for all currently supported options.
 
 module.exports = {
-  config: {
-    // default font size in pixels for all tabs
-    fontSize: 14,
+	config: {
+		// default font size in pixels for all tabs
+		fontSize: 14,
 
-    // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+		// font family with optional fallbacks
+		fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+		// terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+		cursorColor: 'rgba(248,28,229,0.8)',
 
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
+		// `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+		cursorShape: 'BLOCK',
 
-    // set to true for blinking cursor
-    cursorBlink: false,
+		// set to true for blinking cursor
+		cursorBlink: false,
 
-    // color of the text
-    foregroundColor: '#fff',
+		// color of the text
+		foregroundColor: '#fff',
 
-    // terminal background color
-    backgroundColor: '#000',
+		// terminal background color
+		backgroundColor: '#000',
 
-    // border color (window, tabs)
-    // borderColor: '#333',
-    borderColor: 'rgba(248,28,229,0.8)',
+		// border color (window, tabs)
+		// borderColor: '#333',
+		borderColor: 'rgba(248,28,229,0.8)',
 
-    // custom css to embed in the main window
-    css: '.hyper_main { border-width: 2px; }',
+		// custom css to embed in the main window
+		css: '.hyper_main { border-width: 2px; }',
 
-    // custom css to embed in the terminal window
-    termCSS: '',
+		// custom css to embed in the terminal window
+		termCSS: '',
 
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
+		// set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+		// default: `false` on Linux, `true` on Windows (ignored on macOS)
+		showHamburgerMenu: '',
 
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
+		// set to `false` if you want to hide the minimize, maximize and close buttons
+		// additionally, set to `'left'` if you want them on the left, like in Ubuntu
+		// default: `true` on windows and Linux (ignored on macOS)
+		showWindowControls: '',
 
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+		// custom padding (css format, i.e.: `top right bottom left`)
+		padding: '12px 14px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
+		// the full list. if you're going to provide the full color palette,
+		// including the 6 x 6 color cubes and the grayscale map, just provide
+		// an array here instead of a color map object
+		colors: {
+			black: '#000000',
+			red: '#ff0000',
+			green: '#33ff00',
+			yellow: '#ffff00',
+			blue: '#0066ff',
+			magenta: '#cc00ff',
+			cyan: '#00ffff',
+			white: '#d0d0d0',
+			lightBlack: '#808080',
+			lightRed: '#ff0000',
+			lightGreen: '#33ff00',
+			lightYellow: '#ffff00',
+			lightBlue: '#0066ff',
+			lightMagenta: '#cc00ff',
+			lightCyan: '#00ffff',
+			lightWhite: '#ffffff'
+		},
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    // make sure to use a full path if the binary name doesn't work
-    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
-    // if you're using powershell, make sure to remove the `--login` below
-    shell: '/usr/local/bin/bash',
+		// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+		// if left empty, your system's login shell will be used by default
+		// make sure to use a full path if the binary name doesn't work
+		// (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
+		// if you're using powershell, make sure to remove the `--login` below
+		shell: '/usr/local/bin/bash',
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    shellArgs: ['--login'],
+		// for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+		// by default ['--login'] will be used
+		shellArgs: ['--login'],
 
-    // for environment variables
-    env: {},
+		// for environment variables
+		env: {},
 
-    // set to false for no bell
-    bell: 'SOUND',
+		// set to false for no bell
+		bell: 'SOUND',
 
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
+		// if true, selected text will automatically be copied to the clipboard
+		copyOnSelect: false,
 
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
+		// if true, on right click selected text will be copied or pasted if no
+		// selection is present (true by default on Windows)
+		// quickEdit: true
 
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
+		// URL to custom bell
+		// bellSoundURL: 'http://example.com/bell.mp3',
 
-    // for advanced config flags please refer to https://hyper.is/#cfg
-  },
+		// for advanced config flags please refer to https://hyper.is/#cfg
+	},
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: ['hyper-alt-click', 'hyper-dracula', "hyper-broadcast"],
+	// a list of plugins to fetch and install from npm
+	// format: [@org/]project[#version]
+	// examples:
+	//	 `hyperpower`
+	//	 `@company/project`
+	//	 `project#1.0.1`
+	plugins: ['hyper-alt-click', 'hyper-dracula', "hyper-broadcast"],
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: []
+	// in development, you can create a directory under
+	// `~/.hyper_plugins/local/` and include it here
+	// to load it and avoid it being `npm install`ed
+	localPlugins: []
 };
