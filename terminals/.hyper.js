326238ad36233617cf2c4a00eb86ecbefc66fab1
Yochem van Rosmalen <yochem+git@icloud.com>
2018-05-24 22:59:29 +0200

    hyperterm-paste plugin

--- terminals/.hyper.js
+++ terminals/.hyper.js
@@ -105,7 +105,7 @@ module.exports = {
 	//	 `hyperpower`
 	//	 `@company/project`
 	//	 `project#1.0.1`
-	plugins: ['hyper-alt-click', 'hyper-dracula', "hyper-broadcast"],
+	plugins: ['hyper-alt-click', 'hyper-dracula', "hyper-broadcast", "hyperterm-paste"],
 
 	// in development, you can create a directory under
 	// `~/.hyper_plugins/local/` and include it here
