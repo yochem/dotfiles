715c5f83fc1f0cf7f2149a529d98725b742de84b
Yochem van Rosmalen <yochem@users.noreply.github.com>
2019-01-03 19:29:17 +0100

    Add comments

--- .vim/clear_shell.sh
+++ .vim/clear_shell.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# clear screen
 clear
-shift # strip the -c that vim adds to bash from arguments to this script
+
+# strip the -c that vim adds to bash from arguments to this script
+shift
+
+# to use aliases and functions etc.
 . ${HOME}/.bash_profile
+
+# run the given command
 eval $@
