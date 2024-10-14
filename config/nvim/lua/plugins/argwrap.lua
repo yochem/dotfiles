334e6d81ac91114960557edf626dabed3254a109
Yochem van Rosmalen <git@yochem.nl>
2024-10-14 14:17:57 +0200

    argwrap -> mini.splitjoin

--- config/nvim/lua/plugins/argwrap.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"FooSoft/vim-argwrap",
-	init = function()
-		vim.keymap.set("n", "<leader>a", vim.cmd.ArgWrap, { silent = true })
-	end,
-	cmd = "ArgWrap",
-}
---
+++ config/nvim/lua/plugins/mini.splitjoin.lua
@@ -0,0 +1,8 @@
+return {
+	"echasnovski/mini.splitjoin",
+	opts = {
+		mappings = {
+			toggle = '<leader>a',
+		},
+	},
+}
