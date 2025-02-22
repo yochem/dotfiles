66609a372d2f1af2e0ef1963400785e161585169
Yochem van Rosmalen <git@yochem.nl>
2025-02-22 16:36:31 +0100

    Use my own echo diagnostics plugin

---
+++ config/nvim/lua/plugins/chime.lua
@@ -0,0 +1,3 @@
+return {
+	dir = '~/Documents/chime.nvim'
+}
--- config/nvim/plugin/diagnostics.lua
+++ config/nvim/plugin/diagnostics.lua
@@ -11,4 +11,22 @@ vim.diagnostic.config({
 	virtual_text = false,
 	update_in_insert = false,
 	severity_sort = true,
+	chime = {
+		--- @param d vim.Diagnostic
+		--- @return { [1]: string, [2]: string? }[]
+		format = function(d)
+			local sev = vim.diagnostic.severity[d.severity]
+			-- Capitalize name
+			local severity_name = sev:gsub("^(.)(%a*)", function(first, rest)
+				return first:upper() .. rest:lower()
+			end)
+
+			return {
+				{ severity_name, "Diagnostic" .. severity_name},
+				{ ": " },
+				{ vim.split(d.message, "\n")[1] },
+				{ " (" .. d.source .. ")", "LineNr" },
+			}
+		end
+	}
 })
