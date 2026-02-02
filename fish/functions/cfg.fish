4d128b36201854e11eb57af2e5ddea78f149917b
Yochem van Rosmalen <git@yochem.nl>
2026-02-02 18:57:26 +0100

    remove old config

--- fish/functions/cfg.fish
+++ fish/functions/cfg.fish
@@ -1,7 +1,7 @@
-function cfg()
-	cfgdir="$HOME/Documents/dotfiles/config/"
-	fzfopts='--height 40% --layout reverse --select-1 --exit-0'
-	fdopts='--max-depth 5 --type file --relative-path --full-path'
+function cfg
+	set cfgdir "$HOME/Documents/dotfiles/config/"
+	set fzfopts "--height 40% --layout reverse --select-1 --exit-0"
+	set fdopts "--max-depth 5 --type file --relative-path --full-path"
 
 	# $1=git, then open ~/.config/git/config
 	[ -f "$cfgdir/$1/config" ] && nvim "$cfgdir/$1/config" && exit
